2f22f4845a9f3fdcd82e8f57cda931894b31ce66
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-06-30 11:40:09 +0500

    feat: generate pages ShippingMethod

---
+++ src/pages/shipping-methods/[shippingMethodId].tsx
@@ -0,0 +1,73 @@
+import { Suspense } from "react"
+import { Routes } from "@blitzjs/next"
+import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useQuery, useMutation } from "@blitzjs/rpc"
+import { useParam } from "@blitzjs/next"
+
+import Layout from "src/core/layouts/Layout"
+import getShippingMethod from "src/shipping-methods/queries/getShippingMethod"
+import deleteShippingMethod from "src/shipping-methods/mutations/deleteShippingMethod"
+
+export const ShippingMethod = () => {
+  const router = useRouter()
+  const shippingMethodId = useParam("shippingMethodId", "number")
+  const [deleteShippingMethodMutation] = useMutation(deleteShippingMethod)
+  const [shippingMethod] = useQuery(getShippingMethod, {
+    id: shippingMethodId,
+  })
+
+  return (
+    <>
+      <Head>
+        <title>ShippingMethod {shippingMethod.id}</title>
+      </Head>
+
+      <div>
+        <h1>ShippingMethod {shippingMethod.id}</h1>
+        <pre>{JSON.stringify(shippingMethod, null, 2)}</pre>
+
+        <Link
+          href={Routes.EditShippingMethodPage({
+            shippingMethodId: shippingMethod.id,
+          })}
+        >
+          Edit
+        </Link>
+
+        <button
+          type="button"
+          onClick={async () => {
+            if (window.confirm("This will be deleted")) {
+              await deleteShippingMethodMutation({ id: shippingMethod.id })
+              await router.push(Routes.ShippingMethodsPage())
+            }
+          }}
+          style={{ marginLeft: "0.5rem" }}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  )
+}
+
+const ShowShippingMethodPage = () => {
+  return (
+    <div>
+      <p>
+        <Link href={Routes.ShippingMethodsPage()}>ShippingMethods</Link>
+      </p>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <ShippingMethod />
+      </Suspense>
+    </div>
+  )
+}
+
+ShowShippingMethodPage.authenticate = true
+ShowShippingMethodPage.getLayout = (page) => <Layout>{page}</Layout>
+
+export default ShowShippingMethodPage
---
+++ src/pages/shipping-methods/index.tsx
@@ -0,0 +1,70 @@
+import { Suspense } from "react"
+import { Routes } from "@blitzjs/next"
+import Head from "next/head"
+import Link from "next/link"
+import { usePaginatedQuery } from "@blitzjs/rpc"
+import { useRouter } from "next/router"
+import Layout from "src/core/layouts/Layout"
+import getShippingMethods from "src/shipping-methods/queries/getShippingMethods"
+
+const ITEMS_PER_PAGE = 100
+
+export const ShippingMethodsList = () => {
+  const router = useRouter()
+  const page = Number(router.query.page) || 0
+  const [{ shippingMethods, hasMore }] = usePaginatedQuery(getShippingMethods, {
+    orderBy: { id: "asc" },
+    skip: ITEMS_PER_PAGE * page,
+    take: ITEMS_PER_PAGE,
+  })
+
+  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
+  const goToNextPage = () => router.push({ query: { page: page + 1 } })
+
+  return (
+    <div>
+      <ul>
+        {shippingMethods.map((shippingMethod) => (
+          <li key={shippingMethod.id}>
+            <Link
+              href={Routes.ShowShippingMethodPage({
+                shippingMethodId: shippingMethod.id,
+              })}
+            >
+              {shippingMethod.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <button disabled={page === 0} onClick={goToPreviousPage}>
+        Previous
+      </button>
+      <button disabled={!hasMore} onClick={goToNextPage}>
+        Next
+      </button>
+    </div>
+  )
+}
+
+const ShippingMethodsPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>ShippingMethods</title>
+      </Head>
+
+      <div>
+        <p>
+          <Link href={Routes.NewShippingMethodPage()}>Create ShippingMethod</Link>
+        </p>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <ShippingMethodsList />
+        </Suspense>
+      </div>
+    </Layout>
+  )
+}
+
+export default ShippingMethodsPage
