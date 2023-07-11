c18a3d31bed8cc606be16adfd82e02c41c1bce01
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-07-11 23:40:43 +0500

    feat: add enums

---
+++ src/core/enums/StatusColorsEnum.ts
@@ -0,0 +1,9 @@
+export const StatusColors = {
+  failed: "red",
+  fail: "red",
+  pending: "gray",
+  rejected: "red",
+  approved: "green",
+  paying: "green",
+  paid: "green",
+}
---
+++ src/core/enums/StatusEnum.ts
@@ -0,0 +1,9 @@
+export const Statuses = {
+  failed: "Отклонено",
+  fail: "Отклонено",
+  pending: "В обработке",
+  rejected: "Отклонено",
+  approved: "Одобрено",
+  paying: "В процессе выплаты",
+  paid: "Выплачено",
+}
