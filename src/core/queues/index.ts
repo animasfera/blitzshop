451844436c806e13329418ffecda9f04eb4f557c
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-07-17 11:06:36 +0500

    feat: add queses-mails

--- src/core/queues/index.ts
+++ src/core/queues/index.ts
@@ -1,16 +1,16 @@
-import { initSlotsCron } from "./slots"
-import { initTransactionsCron } from "./transactions"
-import { initReportsCron } from "./reports"
+// import { initSlotsCron } from "./slots"
+// import { initTransactionsCron } from "./transactions"
+// import { initReportsCron } from "./reports"
 import { initInvoicesCron } from "./invoices"
-import { initMailsNewsletterCron } from "./mailsNewsletter"
+// import { initMailsNewsletterCron } from "./mailsNewsletter"
 
 export { MailsQueue } from "./mails"
 export { InvoicesQueue } from "./invoices"
-export { SlotsQueue } from "./slots"
-export { TransactionsQueue } from "./transactions"
-export { ReportsQueue } from "./reports"
+// export { SlotsQueue } from "./slots"
+// export { TransactionsQueue } from "./transactions"
+// export { ReportsQueue } from "./reports"
 export { ImagesQueue } from "./images"
-export { MailsNewsletterQueue } from "./mailsNewsletter"
+// export { MailsNewsletterQueue } from "./mailsNewsletter"
 
 export const init = () => {
   console.log("Starting Queues")
@@ -18,9 +18,9 @@ export const init = () => {
 
 export const initCron = () => {
   console.log("Starting Cron Jobs")
-  initSlotsCron()
-  initTransactionsCron()
-  initReportsCron()
+  // initSlotsCron()
+  // initTransactionsCron()
+  // initReportsCron()
   initInvoicesCron()
-  initMailsNewsletterCron()
+  // initMailsNewsletterCron()
 }
