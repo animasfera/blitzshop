6f59cbb3433ac0ac33f61d51454c2292d6c40b49
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-07-21 17:17:21 +0500

    feat: transfering queries transactions

---
+++ src/transactions/mutations/createManyTransactions.ts
@@ -0,0 +1,15 @@
+import { resolver } from "@blitzjs/rpc"
+import { z } from "zod"
+
+import { CreateTransactionSchema } from "../schemas"
+
+export default resolver.pipe(
+  resolver.zod(z.object({ transactions: CreateTransactionSchema.array() })),
+  async (input) => {
+    const sum = function (array: any[], key: string) {
+      return array.reduce((a, b) => a + (b[key] || 0), 0)
+    }
+
+    return true
+  }
+)
