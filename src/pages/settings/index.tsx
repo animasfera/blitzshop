266949c1e5beb6be101b928c4f6a1420fed885e7
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-07-23 17:27:51 +0500

    feat: transfering settings page

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,67 @@
+import React from "react"
+import { invalidateQuery, useMutation } from "@blitzjs/rpc"
+import Head from "next/head"
+import { Box, Heading } from "@chakra-ui/react"
+import { useTranslation } from "react-i18next"
+
+import { Loading } from "src/core/components/Loading"
+
+import { SettingsMenu } from "src/settings/components/SettingsMenu"
+import Layout from "src/core/layouts/Layout"
+import { UserSettingsForm } from "src/users/components/UserSettingsForm"
+import { UserSettingsGuideForm } from "src/users/components/UserSettingsGuideForm"
+import getCurrentUser from "src/users/queries/getCurrentUser"
+import updateUser from "src/users/mutations/updateUser"
+import { UpdateUserSchema } from "src/users/schemas"
+import { useCurrentUser } from "src/core/hooks/useCurrentUser"
+
+const SettingsPage = () => {
+  const { t } = useTranslation(["pages.settings"])
+  const currentUser = useCurrentUser()
+  const [updateUserMutation] = useMutation(updateUser)
+
+  return (
+    <>
+      <Head>
+        <title>{t("index.headers.main")}</title>
+      </Head>
+      <SettingsMenu>
+        <Heading size={"md"}>{t("index.headers.main")}</Heading>
+        {/*<Container maxW={"container.sm"}>*/}
+        <Box maxW={"600px"}>
+          <Loading>
+            <UserSettingsForm
+              schema={UpdateUserSchema}
+              initialValues={currentUser!}
+              onSubmit={async (values) => {
+                await updateUserMutation(values)
+                void invalidateQuery(getCurrentUser)
+                /*
+                if (values.locale && values.locale !== i18n.resolvedLanguage) {
+                  void i18n.changeLanguage(values.locale)
+                }
+                */
+              }}
+            />
+            <UserSettingsGuideForm
+              schema={UpdateUserSchema}
+              initialValues={currentUser!}
+              onSubmit={async (values) => {
+                if (confirm(t("pages.settings:profile.headers.confirm"))) {
+                  await updateUserMutation(values)
+                  void invalidateQuery(getCurrentUser)
+                }
+              }}
+            />
+          </Loading>
+        </Box>
+        {/*</Container>*/}
+      </SettingsMenu>
+    </>
+  )
+}
+
+// SettingsPage.authenticate = true
+// SettingsPage.getLayout = (page) => <Layout>{page}</Layout>
+
+export default SettingsPage
