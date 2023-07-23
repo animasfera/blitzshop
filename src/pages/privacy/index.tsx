a2ad143f250f91a6acdf068ae690a761a49790cb
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-07-23 17:23:23 +0500

    feat: transfering privacy page

---
+++ src/pages/privacy/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Container } from "@chakra-ui/react"
+
+import Layout from "src/core/layouts/Layout"
+
+// import PrivacyPolicyRu from "src/legal/RU/privacy/privacy.md"
+// import PrivacyPolicyEn from "src/legal/EN/privacy/privacy.md"
+
+export const PrivacyPolicyPage = () => {
+  return (
+    <Container maxWidth={"container.lg"}>
+      PrivacyPolicyEn
+      {
+        // currentUser?.locale === LocaleEnum.RU ? <PrivacyPolicyRu /> : <PrivacyPolicyEn />
+      }
+    </Container>
+  )
+}
+
+// PrivacyPolicyPage.getLayout = (page) => <Layout>{page}</Layout>
+export default PrivacyPolicyPage
