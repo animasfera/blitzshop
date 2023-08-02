ad47bb17916c13b91ce7ff211b9b9908faa87c2d
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-08-02 06:42:04 +0500

    feat: create ProductPolicies

--- src/products/components/ProductController.tsx
+++ src/products/components/ProductController.tsx
@@ -1,77 +1,12 @@
 import { useQuery } from "@blitzjs/rpc"
 import { useParam } from "@blitzjs/next"
-import { CurrencyDollarIcon, GlobeAmericasIcon } from "@heroicons/react/24/outline"
 
 import { ProductImages } from "src/products/components/ProductImages"
 import { ProductVariant } from "src/products/components/ProductVariant"
 import { ProductReviews } from "src/products/components/ProductReviews"
 import { ProductDetails } from "src/products/components/ProductDetails"
-
-import { classNames } from "src/core/helpers/classNames"
+import { ProductPolicies } from "src/products/components/ProductPolicies"
 import getItem from "src/items/queries/getItem"
-
-const product = {
-  name: "Basic Tee",
-  price: "$35",
-  rating: 3.9,
-  reviewCount: 512,
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Women", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      id: 1,
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
-      imageAlt: "Back of women's Basic Tee in black.",
-      primary: true,
-    },
-    {
-      id: 2,
-      imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
-      imageAlt: "Side profile of women's Basic Tee in black.",
-      primary: false,
-    },
-    {
-      id: 3,
-      imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
-      imageAlt: "Front of women's Basic Tee in black.",
-      primary: false,
-    },
-  ],
-  colors: [
-    { name: "Black", bgColor: "bg-gray-900", selectedColor: "ring-gray-900" },
-    { name: "Heather Grey", bgColor: "bg-gray-400", selectedColor: "ring-gray-400" },
-  ],
-  sizes: [
-    { name: "XXS", inStock: true },
-    { name: "XS", inStock: true },
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-    { name: "XL", inStock: false },
-  ],
-  description: `
-    <p>The Basic tee is an honest new take on a classic. The tee uses super soft, pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally, with a special dye technique that gives each tee it's own look.</p>
-    <p>Looking to stock your closet? The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.</p>
-  `,
-  details: [
-    "Only the best materials",
-    "Ethically and locally made",
-    "Pre-washed and pre-shrunk",
-    "Machine wash cold with similar colors",
-  ],
-}
-const policies = [
-  {
-    name: "International delivery",
-    icon: GlobeAmericasIcon,
-    description: "Get your order in 2 years",
-  },
-  { name: "Loyalty rewards", icon: CurrencyDollarIcon, description: "Don't look at other tees" },
-]
 
 export const ProductController = () => {
   const itemId = useParam("itemId", "number")
@@ -84,7 +19,6 @@ export const ProductController = () => {
           <div className="lg:grid lg:auto-rows-min lg:grid-cols-12 lg:gap-x-8">
             <div className="lg:col-span-5 lg:col-start-8">
               <ProductVariant item={item} />
-              {/* Reviews */}
               <ProductReviews item={item} />
             </div>
 
@@ -111,32 +45,7 @@ export const ProductController = () => {
 
               <ProductDetails item={item} />
 
-              {/* Policies */}
-              <section aria-labelledby="policies-heading" className="mt-10">
-                <h2 id="policies-heading" className="sr-only">
-                  Our Policies
-                </h2>
-
-                <dl className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
-                  {policies.map((policy) => (
-                    <div
-                      key={policy.name}
-                      className="rounded-lg border border-gray-200 bg-gray-50 p-6 text-center"
-                    >
-                      <dt>
-                        <policy.icon
-                          className="mx-auto h-6 w-6 flex-shrink-0 text-gray-400"
-                          aria-hidden="true"
-                        />
-                        <span className="mt-4 text-sm font-medium text-gray-900">
-                          {policy.name}
-                        </span>
-                      </dt>
-                      <dd className="mt-1 text-sm text-gray-500">{policy.description}</dd>
-                    </div>
-                  ))}
-                </dl>
-              </section>
+              <ProductPolicies />
             </div>
           </div>
         </div>
