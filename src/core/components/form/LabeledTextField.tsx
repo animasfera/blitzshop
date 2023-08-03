22a631957fb4e45292e5d9b967c42e7dfab8088e
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-08-03 12:53:23 +0500

    feat: initial text field

--- src/core/components/form/LabeledTextField.tsx
+++ src/core/components/form/LabeledTextField.tsx
@@ -1,5 +1,6 @@
 import { forwardRef, ComponentPropsWithoutRef, PropsWithoutRef } from "react"
 import { useField, UseFieldConfig } from "react-final-form"
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid"
 
 import { Input } from "@chakra-ui/input"
 import { FormControl, FormLabel } from "@chakra-ui/form-control"
@@ -33,19 +34,47 @@ export const LabeledTextField = forwardRef<HTMLInputElement, LabeledTextFieldPro
     const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
 
     return (
-      <FormControl {...outerProps}>
-        <FormLabel {...labelProps}>
-          {label}
-          <Input {...input} disabled={submitting} {...props} ref={ref} />
-        </FormLabel>
-        {touched && normalizedError && (
-          <div role="alert" style={{ color: "red" }}>
-            {normalizedError}
+      <>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email
+          </label>
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+              placeholder="you@example.com"
+              defaultValue="adamwathan"
+              aria-invalid="true"
+              aria-describedby="email-error"
+            />
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
+            </div>
           </div>
-        )}
-      </FormControl>
+          <p className="mt-2 text-sm text-red-600" id="email-error">
+            Not a valid email address.
+          </p>
+        </div>
+      </>
     )
   }
 )
 
 export default LabeledTextField
+
+/*
+  <FormControl {...outerProps}>
+    <FormLabel {...labelProps}>
+      {label}
+      <Input {...input} disabled={submitting} {...props} ref={ref} />
+    </FormLabel>
+    {touched && normalizedError && (
+      <div role="alert" style={{ color: "red" }}>
+        {normalizedError}
+      </div>
+    )}
+  </FormControl>
+*/
