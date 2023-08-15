67267742bec3c55bb542af88cc6274b6c97bf123
Roman Stepanov <ketayo1@gmail.com>
2023-08-15 15:45:28 +0800

    feat(tailwind): create container component

---
+++ src/core/tailwind-ui/application-ui/Container.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react"
+
+type ContainerProps = {
+  size: "lg" | "sm"
+  children: ReactElement[] | ReactElement
+}
+export default function Container({ children, size }: ContainerProps) {
+  return (
+    <>
+      <div
+        className={"container mx-auto px-4 " + (size === "sm" ? "max-w-3xl" : "sm:px-6 lg:px-8")}
+      >
+        {children}
+      </div>
+    </>
+  )
+}
