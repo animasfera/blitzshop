d8df78a1e1a08d86b235f3fe8ea30a8b4370c36a
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-08-16 15:01:44 +0500

    fix(fonts): update fonts in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 
-/*
-Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
-"Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
-"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"
-
-font-family: 'Inter', sans-serif;
-font-family: 'Roboto', sans-serif;
-font-family: 'Helvetica Neue', sans-serif;
-*/
-
 module.exports = {
   content: ["./{src,app,pages}/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    /*
-    extend: {
-      fontFamily: {
-        inter:
-      }
-    },
-    */
     fontFamily: {
-      body: ["'Inter'", "'Roboto'"],
+      body: [
+        "'Inter'",
+        "'Roboto'",
+        "sans-serif",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "Noto Sans",
+        "'Arial'",
+        "Segoe UI",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
       sans: [
         "'Inter'",
         "'Roboto'",
