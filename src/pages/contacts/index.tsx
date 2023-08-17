ac3bfd7566b4d28bc28a362b4677acb534d22455
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-08-17 16:45:38 +0500

    feat(pages): add ContactsPage

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -5,6 +5,9 @@ import { useTranslation } from "react-i18next"
 import Layout from "src/core/layouts/Layout"
 import Container from "src/core/tailwind-ui/application-ui/Container"
 import HeadingPage from "src/core/tailwind-ui/headings/HeadingPage"
+import HeadingSection from "src/core/tailwind-ui/headings/HeadingSection"
+import HeadingBlock from "src/core/tailwind-ui/headings/HeadingBlock"
+
 import { nl2br } from "src/core/helpers/Helpers"
 
 interface ContactsPageProps {}
@@ -17,7 +20,30 @@ export const ContactsPage: BlitzPage = (props: ContactsPageProps) => {
   return (
     <Layout title={t("index.headers.main")}>
       <Container size={"sm"}>
-        <HeadingPage title={t("index.headers.main")} />
+        <div className="flex flex-col gap-4">
+          <HeadingPage title={t("index.headers.main")} />
+
+          <p className="m-0 mb-2">
+            {`${t("index.texts.contactUsByEmail")}: `}
+            <span>
+              <a href={"mailto:" + process.env.NEXT_PUBLIC_SITE_EMAIL}>
+                {process.env.NEXT_PUBLIC_SITE_EMAIL}
+              </a>
+            </span>
+          </p>
+
+          <HeadingSection title={t("index.headers.businessDetails")} />
+
+          <div>
+            <HeadingBlock title={t("index.headers.netherlands")} />
+            <div>{nl2br(t("index.texts.businessDetailsEu"), false)}</div>
+          </div>
+
+          <div>
+            <HeadingBlock title={t("index.headers.russia")} />
+            <div>{nl2br(t("index.texts.businessDetailsRu"), false)}</div>
+          </div>
+        </div>
       </Container>
     </Layout>
   )
@@ -26,62 +52,13 @@ export const ContactsPage: BlitzPage = (props: ContactsPageProps) => {
 export default ContactsPage
 
 /*
-    <>
-      <Heading mb={6}>{t("index.headers.main")}</Heading>
-
-      <Stack spacing={5} mb={10}>
-        <HStack spacing={2}>
-          <Box textAlign={"right"}>{t("index.texts.contactUsByEmail")}:</Box>
-          <Box textAlign={"left"}>
-            <a href={"mailto:" + process.env.NEXT_PUBLIC_SITE_EMAIL}>
-              {process.env.NEXT_PUBLIC_SITE_EMAIL}
-            </a>
-          </Box>
-        </HStack>
-      </Stack>
-
-      <Heading size={"md"}>{t("index.headers.businessDetails")}</Heading>
-
-      <Heading size={"sm"} mb={1}>
-        {t("index.headers.netherlands")}
-      </Heading>
-      <Box>{nl2br(t("index.texts.businessDetailsEu"), false)}</Box>
-
-      {i18n.resolvedLanguage === "ru" && (
-        <>
-          <Heading size={"sm"} mt={6} mb={1}>
-            {t("index.headers.russia")}
-          </Heading>
-          <Box>{nl2br(t("index.texts.businessDetailsRu"), false)}</Box>
-        </>
-      )}
-    </>
-    */
-
-/*
-import { useRouter } from "next/router"
-import Layout from "src/core/layouts/Layout"
-import { SignupForm } from "src/auth/components/SignupForm"
-import { BlitzPage, Routes } from "@blitzjs/next"
-import { useTranslation } from "react-i18next"
-
-import { Loading } from "src/core/components/Loading"
-import Container from "../core/tailwind-ui/application-ui/Container"
-
-const SignupPage: BlitzPage = () => {
-  const router = useRouter()
-
-  const { t } = useTranslation(["pages.signup"])
-
-  return (
-    <Layout title={t("title")}>
-      <Container size={"sm"}>
-        <SignupForm onSuccess={() => router.push(Routes.Home())} />
-      </Container>
-    </Layout>
-  )
-}
-
-export default SignupPage
-
+TODO:
+<>
+  {i18n.resolvedLanguage === "ru" && (
+    <div>
+        <HeadingBlock title={t("index.headers.russia")} />
+        <div>{nl2br(t("index.texts.businessDetailsRu"), false)}</div>
+      </div>
+  )}
+</>
 */
