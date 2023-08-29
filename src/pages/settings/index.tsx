7e014b1efa901340c8e185ac694f1c401ffde03e
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-08-29 12:32:34 +0500

    feat(pages): create SettingsPage

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,70 +1,23 @@
 "use client"
 import React from "react"
 import { BlitzPage } from "@blitzjs/next"
-import { invalidateQuery, useMutation } from "@blitzjs/rpc"
-import Head from "next/head"
-import { Box, Heading } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 
+import { Layout } from "src/core/layouts/Layout"
 import { Loading } from "src/core/components/Loading"
+import { SettingsUserController } from "src/settings/components/SettingsUserController"
 
-import { SettingsMenu } from "src/settings/components/SettingsMenu"
-import Layout from "src/core/layouts/Layout"
-import { UserSettingsForm } from "src/users/components/UserSettingsForm"
-import { UserSettingsGuideForm } from "src/users/components/UserSettingsGuideForm"
-import getCurrentUser from "src/users/queries/getCurrentUser"
-import updateUser from "src/users/mutations/updateUser"
-import { UpdateUserSchema } from "src/users/schemas"
-import { useCurrentUser } from "src/core/hooks/useCurrentUser"
-
-const SettingsPage: BlitzPage = () => {
-  return <>SettingsPage</>
-  /*
+export const SettingsPage: BlitzPage = () => {
   const { t } = useTranslation(["pages.settings"])
-  const currentUser = useCurrentUser()
-  const [updateUserMutation] = useMutation(updateUser)
 
   return (
-    <>
-      <Head>
-        <title>{t("index.headers.main")}</title>
-      </Head>
-      <SettingsMenu>
-        <Heading size={"md"}>{t("index.headers.main")}</Heading>
-        <Box maxW={"600px"}>
-          <Loading>
-            <UserSettingsForm
-              schema={UpdateUserSchema}
-              initialValues={currentUser!}
-              onSubmit={async (values) => {
-                await updateUserMutation(values)
-                void invalidateQuery(getCurrentUser)
-
-                // if (values.locale && values.locale !== i18n.resolvedLanguage) {
-                //   void i18n.changeLanguage(values.locale)
-                // }
-
-              }}
-            />
-            <UserSettingsGuideForm
-              schema={UpdateUserSchema}
-              initialValues={currentUser!}
-              onSubmit={async (values) => {
-                if (confirm(t("pages.settings:profile.headers.confirm"))) {
-                  await updateUserMutation(values)
-                  void invalidateQuery(getCurrentUser)
-                }
-              }}
-            />
-          </Loading>
-        </Box>
-      </SettingsMenu>
-    </>
+    <Layout title={t("index.title")}>
+      <Loading>
+        <SettingsUserController />
+      </Loading>
+    </Layout>
   )
-  */
 }
 
-// SettingsPage.authenticate = true
-// SettingsPage.getLayout = (page) => <Layout>{page}</Layout>
-
+export { getServerSideProps } from "src/core/getServerSideProps"
 export default SettingsPage
