fae4651f878232e359c95e4ff7884dc44188bc82
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-09-28 20:27:41 +0200

    feat(cart): update cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next"
 import { useSession } from "@blitzjs/auth"
 import Layout from "../../core/layouts/Layout"
 import React from "react"
+import { CartController } from "src/carts/components/CartController"
 
 const CartPage = (props) => {
   const { t } = useTranslation()
@@ -11,6 +12,7 @@ const CartPage = (props) => {
       <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         {t("cart:header")}
       </h1>
+
       {/*<Loading>{session.userId && <CartController userId={session.userId} />}</Loading>*/}
     </>
   )
