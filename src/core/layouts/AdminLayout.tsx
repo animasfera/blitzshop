d36c6fdd321b79875bca12787680dd27f883dd4e
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-10-14 21:15:10 +0200

    fix(components): update Admin Layout and container

--- src/core/layouts/AdminLayout.tsx
+++ src/core/layouts/AdminLayout.tsx
@@ -1,67 +1,40 @@
-/*
-import Head from "next/head"
-import { useRouter } from "next/router"
-import { useSession } from "@blitzjs/auth"
-import { BlitzLayout, Routes } from "@blitzjs/next"
-import { Box } from "@chakra-ui/react"
-import { UserRoleEnum } from "@prisma/client"
-
-import AdminSidebar from "src/core/components/sections/AdminSidebar"
-import { Loading } from "src/core/components/Loading"
-
-const AdminLayout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
-  title,
-  children,
-}) => {
-  const session = useSession()
-  const router = useRouter()
-
-
-  if (session.role !== UserRoleEnum.ADMIN) {
-    // void router.push(Routes.Home())
-  }
-
-  return (
-    <>
-      <Head>
-        <title>{title || "shop"}</title>
-      </Head>
-
-     <AdminSidebar>
-      <Box maxW={"container.xl"} pt={4} pb={12} pl={6} pr={4}>
-        <Loading>{children}</Loading>
-      </Box>
-      </AdminSidebar>
-    </>
-  )
-}
-*/
-
 import Head from "next/head"
 import React, { useEffect, FC } from "react"
 import { BlitzLayout } from "@blitzjs/next"
-import { Box, Container } from "@chakra-ui/react"
 
-import { Loading } from "src/core/components/Loading"
+import { Container } from "src/core/tailwind-ui/application-ui/Container"
 import i18n from "../i18n"
 
-const AdminLayout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
-  title,
-  children,
-}) => {
+interface LayoutProps {
+  title?: string
+  styles?: string
+  children?: React.ReactNode
+}
+
+export const AdminLayout: BlitzLayout<LayoutProps> = (props) => {
+  const { title, styles, children } = props
+
+  useEffect(() => {
+    // @ts-ignore
+    document.documentElement.lang = i18n.resolvedLanguage?.toUpperCase()
+  }, [])
+
   return (
-    <Box>
+    <div>
       <Head>
         <title>{title || "shop"}</title>
-        {
-          // <link rel="icon" href="/favicon.ico" />
-        }
+        <link rel="icon" href="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" />
       </Head>
 
-      <Loading>{children}</Loading>
-    </Box>
+      <main>
+        {
+          // TODO: add AdminSidebar
+        }
+        <Container styles={styles}>{children}</Container>
+      </main>
+    </div>
   )
 }
 
-// AdminLayout.authenticate = true
+AdminLayout.authenticate = true
 export default AdminLayout
