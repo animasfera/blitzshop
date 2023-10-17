7be29dce5bf8c5dd162c5ac3ebdc6e4fb365645d
Mikhail Koryukov <mkdir-dev@yandex.ru>
2023-10-17 16:51:15 +0200

    fix(components): fix conflict

--- src/core/layouts/AdminLayout.tsx
+++ src/core/layouts/AdminLayout.tsx
@@ -1,44 +1,4 @@
 import Head from "next/head"
-<<<<<<< HEAD
-import React, { useEffect, FC } from "react"
-import { BlitzLayout } from "@blitzjs/next"
-
-import { Container } from "src/core/tailwind-ui/application-ui/Container"
-import i18n from "../i18n"
-
-interface LayoutProps {
-  title?: string
-  styles?: string
-  children?: React.ReactNode
-}
-
-export const AdminLayout: BlitzLayout<LayoutProps> = (props) => {
-  const { title, styles, children } = props
-
-  useEffect(() => {
-    // @ts-ignore
-    document.documentElement.lang = i18n.resolvedLanguage?.toUpperCase()
-  }, [])
-
-  return (
-    <div>
-      <Head>
-        <title>{title || "shop"}</title>
-        <link rel="icon" href="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" />
-      </Head>
-
-      <main>
-        {
-          // TODO: add AdminSidebar
-        }
-        <Container styles={styles}>{children}</Container>
-      </main>
-    </div>
-  )
-}
-
-AdminLayout.authenticate = true
-=======
 import { BlitzLayout } from "@blitzjs/next"
 import { useSession } from "@blitzjs/auth"
 import AdminSidebar from "../tailwind-ui/application-ui/admin/AdminSidebar"
@@ -87,5 +47,4 @@ export const AdminLayout: BlitzLayout<AdminLayoutProps> = (props) => {
   )
 }
 
->>>>>>> b844bff2fa7542b70c27be0cbd7fc67fc3908587
 export default AdminLayout
