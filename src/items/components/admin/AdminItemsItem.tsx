3f3254a2d75b99a1ac30a69739eae05a3400ef22
Roman Shtepin <videolimiter@gmail.com>
2023-10-17 13:04:38 +0500

    fix: table ref: SHOP-T-75

--- src/items/components/admin/AdminItemsItem.tsx
+++ src/items/components/admin/AdminItemsItem.tsx
@@ -31,30 +31,24 @@ const AdminItemsItem = (props: IAdminItemsItem) => {
   const { item } = props
   return (
     <tr>
-      <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{item.id}</td>
-      <td className="hidden  w-40 px-3 py-2 text-sm text-gray-500 lg:table-cell">
+      <td className="max-w-fit px-3 py-4 text-sm text-gray-500 lg:table-cell">{item.id}</td>
+      <td className="px-3 py-2 text-sm text-gray-500 lg:table-cell">
         <img
           className="h-20 w-20 rounded-md object-cover object-center"
           src={item.coverImage.image.url}
           alt=""
         />
       </td>
-      <td className="px-3 py-1 text-sm text-gray-500 sm:table-cell">
+      <td className="px-3 py-1 text-sm text-black-600 lg:font-normal sm:table-cell sm:font-semibold">
+        {item.title}
         <dl className="font-normal lg:hidden">
           <dt className="sr-only sm:hidden">Картинка</dt>
-          <dd className="mt-1 truncate text-gray-500 ">
-            <img
-              className="h-20 w-20 rounded-md object-cover object-center"
-              src={item.coverImage.image.url}
-              alt=""
-            />
-          </dd>
+          <dd className="mt-1 truncate text-gray-500 ">{item.category?.titleRu}</dd>
         </dl>
-        {item.title}
       </td>
-      <td className=" py-4 text-sm text-gray-500">{item.category?.titleRu}</td>
+      <td className="hidden py-4 text-sm text-gray-500 ">{item.category?.titleRu}</td>
       <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{item.status}</td>
-      <td className="px-3 py-4 text-sm text-gray-500">{item.qty}</td>
+      <td className="px-3 py-4 text-sm text-gray-500 sm:text-center lg:text-left">{item.qty}</td>
       <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
         <a href="#" className="text-indigo-600 hover:text-indigo-900">
           Edit<span className="sr-only">, {item.title}</span>
--- src/items/components/admin/AdminItemsList.tsx
+++ src/items/components/admin/AdminItemsList.tsx
@@ -8,32 +8,30 @@ export interface IAdminItemsList {
 const AdminItemsList = (props: IAdminItemsList) => {
   const { items } = props
   return (
-    <div className="px-4 sm:px-4 lg:px-4">
-      <div className="-mx-4 mt-8 sm:-mx-0">
+    <div className="sm:px-0 lg:px-4">
+      <div className="lg:mt-8 sm:mt-0 sm:-mx-0">
         <table className="min-w-full divide-y divide-gray-300">
           <thead>
             <tr>
               <th
                 scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                className="max-w-fit px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
               >
-                id
+                #
               </th>
               <th
                 scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                className="px-3 py-3.5 lg:text-left sm:text-center text-sm font-semibold text-gray-900 lg:table-cell"
               >
                 Картинка
               </th>
               <th
                 scope="col"
                 className="py-3.5 px-3 pr-1 text-left text-sm font-semibold text-gray-900 "
-              >
-                Название
-              </th>
+              ></th>
               <th
                 scope="col"
-                className="hidden py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                className="hidden py-3.5 text-left text-sm font-semibold text-gray-900 "
               >
                 Категория
               </th>
@@ -43,8 +41,11 @@ const AdminItemsList = (props: IAdminItemsList) => {
               >
                 Статус
               </th>
-              <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                Количество
+              <th
+                scope="col"
+                className="px-3 py-3.5 lg:text-left sm:text-center text-sm font-semibold text-gray-900 w-fit"
+              >
+                Кол-во
               </th>
 
               <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
