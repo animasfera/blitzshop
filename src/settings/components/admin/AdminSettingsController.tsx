d42396b882e4476d3c0154220b7f0a73a098c4d5
Roman Shtepin <videolimiter@gmail.com>
2023-10-20 17:34:33 +0500

    fix: change mutations and queries ref: SHOP-T-21

--- src/settings/components/admin/AdminSettingsController.tsx
+++ src/settings/components/admin/AdminSettingsController.tsx
@@ -1,12 +1,18 @@
+import { useMutation, useQuery } from "@blitzjs/rpc"
+import updateConfigs from "src/configs/mutations/updateConfigs"
+import getConfigs from "src/configs/queries/getConfigs"
 import { AdminSettingsForm } from "./AdminSettingsForm"
 
 const AdminSettingsController = () => {
+  const [updateConfigMutation] = useMutation(updateConfigs)
+  const [{ configs }] = useQuery(getConfigs, {})
   return (
     <div>
       <AdminSettingsForm
+        initialValues={configs}
         submitText="Сохранить"
-        onSubmit={(data) => {
-          alert(data)
+        onSubmit={async (data) => {
+          await updateConfigMutation({ configs: data })
         }}
       />
     </div>
