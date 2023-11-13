e2d74e34d9679cae1ebcb58a8eb3b3673b3bd857
Roman Palych <videolimiter@gmail.com>
2023-11-13 15:46:21 +0500

    feat:  remove policy from product/itemId page ref: SHOP-T-97

--- src/items/components/ItemPolicies.tsx
+++ src/items/components/ItemPolicies.tsx
@@ -1,14 +1,3 @@
-import { CurrencyDollarIcon, GlobeAmericasIcon } from "@heroicons/react/24/outline"
-
-const policies = [
-  {
-    name: "International delivery",
-    icon: GlobeAmericasIcon,
-    description: "Get your order in 2 years",
-  },
-  { name: "Loyalty rewards", icon: CurrencyDollarIcon, description: "Don't look at other tees" },
-]
-
 interface ItemPoliciesProps {}
 
 export const ItemPolicies = (props: ItemPoliciesProps) => {
@@ -19,24 +8,6 @@ export const ItemPolicies = (props: ItemPoliciesProps) => {
       <h2 id="policies-heading" className="sr-only">
         Our Policies
       </h2>
-
-      <dl className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
-        {policies.map((policy) => (
-          <div
-            key={policy.name}
-            className="rounded-lg border border-gray-200 bg-gray-50 p-6 text-center"
-          >
-            <dt>
-              <policy.icon
-                className="mx-auto h-6 w-6 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
-              />
-              <span className="mt-4 text-sm font-medium text-gray-900">{policy.name}</span>
-            </dt>
-            <dd className="mt-1 text-sm text-gray-500">{policy.description}</dd>
-          </div>
-        ))}
-      </dl>
     </section>
   )
 }
