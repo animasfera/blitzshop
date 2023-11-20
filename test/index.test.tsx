3bb4f7784ece1f727aa68fc34b3f09e8ea9b266a
Roman Palych <videolimiter@gmail.com>
2023-11-20 13:52:24 +0500

    feat: comment test ref: SHOP-T-99

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -15,18 +15,16 @@ test.skip("renders blitz documentation link", () => {
   // This is an example of how to ensure a specific item is in the document
   // But it's disabled by default (by test.skip) so the test doesn't fail
   // when you remove the default content from the page
-
   // This is an example on how to mock api hooks when testing
-  vi.mock("src/users/hooks/useCurrentUser", () => ({
-    useCurrentUser: () => ({
-      id: 1,
-      name: "User",
-      email: "user@email.com",
-      role: "user",
-    }),
-  }))
-
-  const { getByText } = render(<Home />)
-  const linkElement = getByText(/Blitz Docs/i)
-  expect(linkElement).toBeInTheDocument()
+  // vi.mock("src/users/hooks/useCurrentUser", () => ({
+  //   useCurrentUser: () => ({
+  //     id: 1,
+  //     name: "User",
+  //     email: "user@email.com",
+  //     role: "user",
+  //   }),
+  // }))
+  // const { getByText } = render(<Home />)
+  // const linkElement = getByText(/Blitz Docs/i)
+  // expect(linkElement).toBeInTheDocument()
 })
